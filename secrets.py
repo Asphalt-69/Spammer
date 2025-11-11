fb214891ab89458c1208fd14ea2c4965e607d197
Batman <asph4lt.69@gmail.com>
2025-11-11 11:31:42 +0530

    Tokens placeholder

---
+++ secrets.py
@@ -0,0 +1,8 @@
+# COPY THIS FILE TO secrets.py AND ADD YOUR REAL TOKENS
+# DO NOT COMMIT YOUR REAL TOKENS TO GITHUB!
+
+TOKENS = [
+    "your_token_1_here",
+    "your_token_2_here", 
+    "your_token_3_here"
+]
